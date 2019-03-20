ef77448fbf9f994770bcc8c751e2ba55f993f311
wboyd600 <37971619+wboyd600@users.noreply.github.com>
2019-03-20 19:07:37 -0400

    3/20

--- HashingAnalysis/HashingAnalysis.cpp
+++ HashingAnalysis/HashingAnalysis.cpp
@@ -34,12 +34,138 @@ int GetRandom(int range) {
 	int t = Random(0, range);
 	return t;
 }
-class KeyAnalysis {
+int GetBitCount(int toCount) {
+	int bitCount = 0;
+	while (toCount > 0) {
+		toCount = toCount - pow(2.0, (double)bitCount);
+		bitCount++;
+	}
+	return bitCount;
+}
+float GetLoadFactor(int itemNumber, int tableSize) {
+	return (float)itemNumber / tableSize;
+}
+int MidSquareHashing(int key, int sizeBitCount,int tableSize) {
+	// key range between 0-3*table size
+	// take middle two digits
+	int squared = key * key;
+	int divideBy2 = (GetBitCount(squared) - sizeBitCount) / 2;
+	int divided = squared;
+	if (divideBy2 > 0)
+		divided = divided >> divideBy2;
+	return (divided % tableSize);
+}
+int KeyModTableSize(int key, int tableSize) {
+	return key % tableSize;
+}
+void AddToVector(int hash, vector<int> &ourElements) {
+	int i = hash + 1;
+	bool added = false;
+	while (i < ourElements.size()) {
+		if (ourElements[i] != -1) {
+			i++;
+		}
+		else {
+			ourElements[i] = hash;
+			added = true;
+		}
+	}
+	if (!added) {
+		i = 0;
+		while (i < ourElements.size()) {
+			if (ourElements[i] != -1) {
+				i++;
+			}
+			else {
+				ourElements[i] = hash;
+			}
+		}
+	}
+}
+class KeyAnalysisOpenAdressing {
 public:
-	KeyAnalysis(int tableSize) {
+	KeyAnalysisOpenAdressing(int tableSize) {
 		this->tableSize = tableSize;
 		itemsAdded = 0;
 		collisions = 0;
+		sizeBitCount = GetBitCount(tableSize);
+		for (int i = 0; i < tableSize; i++)
+		{
+			ourElements.push_back(-1);
+		}
+	}
+	int Contains(int hash) {
+		for (int i = 0; i < (int)this->ourElements.size(); i++)
+		{
+			if (ourElements[i] == hash)
+				return i;
+		}
+		return -1;
+	}
+	vector<Node *> GetPointsKeyMod() {
+		vector<Node *> nodes;
+		int range = 3 * this->tableSize;
+		itemsAdded = 0;
+		while (GetLoadFactor(this->itemsAdded, tableSize) < 1.0f) {
+			int random = GetRandom(range);
+			int hash = KeyModTableSize(random, this->tableSize);
+			Node * g = new Node;
+			itemsAdded++;
+			if (hash < tableSize) {
+				int ind = Contains(hash);
+				if (ind == -1) {
+					ourElements[hash] = hash;
+				}
+				else {
+					AddToVector(hash, ourElements);
+					collisions++;
+				}
+			}
+			g->collisionCount = collisions;
+			g->loadFactor = GetLoadFactor(itemsAdded, tableSize);
+			nodes.push_back(g);
+		}
+		return nodes;
+	}
+	vector<Node *> GetPointsMidSquare() {
+		vector<Node *> nodes;
+		int range = 3 * this->tableSize;
+		itemsAdded = 0;
+		while (GetLoadFactor(this->itemsAdded, tableSize) < 1.0f) {
+			int random = GetRandom(range);
+			int hash = MidSquareHashing(random,sizeBitCount, this->tableSize);
+			Node * g = new Node;
+			itemsAdded++;
+			if (hash < tableSize) {
+				int ind = Contains(hash);
+				if (ind == -1) {
+					ourElements[hash] = hash;
+				}
+				else {
+					AddToVector(hash, ourElements);
+					collisions++;
+				}
+			}
+			g->collisionCount = collisions;
+			g->loadFactor = GetLoadFactor(itemsAdded, tableSize);
+			nodes.push_back(g);
+		}
+		return nodes;
+	}
+private:
+	int tableSize;
+	int itemsAdded;
+	int collisions;
+	int sizeBitCount;
+	std::vector<int> ourElements;
+};
+class KeyAnalysisChaining {
+public:
+	KeyAnalysisChaining(int tableSize) {
+		this->tableSize = tableSize;
+		itemsAdded = 0;
+		collisions = 0;	
+		sizeBitCount = GetBitCount(tableSize);
 	}
 	int Contains(int hash) {
 		for (int i = 0; i < (int)this->ourElements.size(); i++)
@@ -50,32 +176,14 @@ public:
 		return -1;
 	}
 
-	int MidSquareHashing(int key) {
-		// key range between 0-3*table size
-		// fit within 100
-		// take middle two digits
-		long squared = key * key;
-		int count = 1;
-		int down = squared / 10;
-		while (down > 0) {
-			count++;
-			down = down / 10;
-		}
-		int current = 0;
-		down = squared;
-		while (count - current > (count / 2 + 1)) {
-			down = down / 10;
-			current++;
-		}
-		return (down % this->tableSize);
-	}
+	
 	vector<Node *> GetPointsMidSquare() {
 		vector<Node *> nodes;
 		int range = 3 * this->tableSize;
 		itemsAdded = 0;
 		while (GetLoadFactor(this->itemsAdded, tableSize) < 1.0f) {
 			int random = GetRandom(range);
-			int hash = KeyModTableSize(random);
+			int hash = MidSquareHashing(random,sizeBitCount,tableSize);
 			Node * g = new Node;
 			itemsAdded++;
 			Element * temp = NULL;
@@ -106,7 +214,7 @@ public:
 		itemsAdded = 0;
 		while (GetLoadFactor(this->itemsAdded, tableSize) < 1.0f) {
 			int random = GetRandom(range);
-			int hash = KeyModTableSize(random);
+			int hash = KeyModTableSize(random,this->tableSize);
 			Node * g = new Node;
 			itemsAdded++;
 			Element * temp = NULL;
@@ -131,40 +239,56 @@ public:
 		}
 		return nodes;
 	}
-	float GetLoadFactor(int itemNumber, int tableSize) {
-		return (float)itemNumber / tableSize;
-	}
-	int KeyModTableSize(int key) {
-		return key % this->tableSize;
-	}
+	
+	
 private:
 	int collisions;
 	int itemsAdded;
 	int tableSize;
+	int keySquaredBitCount;
+	int sizeBitCount;
 	vector<Element *> ourElements;
 };
 
-int GetBitCount(int toCount) {
-	int bitCount = 0;
-	while (toCount > 0) {
-		bitCount = bitCount - (int)pow(2, bitCount);
-		bitCount++;
-	}
-	return bitCount;
-}
+
 
 int main()
 {
 	// initial table size is 100
-	int bit = GetBitCount(100);
-	cout << bit;
-	//keyanalysis guy(10);
-	//vector<node * > toprint = guy.getpointskeymod();
+	//KeyAnalysisChaining guy(1000);
+	//vector<Node * > toprint = guy.GetPointsKeyMod();
 	//for (int i = 0; i < (int) toprint.size(); i++)
 	//{
-	//	cout << "( " << toprint[i]->loadfactor << ", " << toprint[i]->collisioncount << ") ";
+	//	cout << "( " << toprint[i]->loadFactor << ", " << toprint[i]->collisionCount << ") ";
 	//}
-
+	//cout << "\n";
+	KeyAnalysisChaining chain(97);
+	vector<Node * > chainPrint = chain.GetPointsKeyMod();
+	for (size_t i = 0; i < chainPrint.size(); i++)
+	{
+		cout << "( " << chainPrint[i]->loadFactor << ", " << chainPrint[i]->collisionCount << ") ";
+	}
+	cout << "\n" << "\n";;
+	KeyAnalysisChaining chain1(97);
+	vector<Node * > chainPrint1 = chain1.GetPointsMidSquare();
+	for (size_t i = 0; i < chainPrint1.size(); i++)
+	{
+		cout << "( " << chainPrint1[i]->loadFactor << ", " << chainPrint1[i]->collisionCount << ") ";
+	}
+	cout << "\n" << "\n";;
+	KeyAnalysisOpenAdressing guy1(97);
+	vector<Node * > toprint1 = guy1.GetPointsKeyMod();
+	for (int i = 0; i < (int)toprint1.size(); i++)
+	{
+		cout << "( " << toprint1[i]->loadFactor << ", " << toprint1[i]->collisionCount << ") ";
+	}
+	cout << "\n" << "\n";;
+	KeyAnalysisOpenAdressing guy(97);
+	vector<Node * > toprint = guy.GetPointsMidSquare();
+	for (int i = 0; i < (int)toprint1.size(); i++)
+	{
+		cout << "( " << toprint[i]->loadFactor << ", " << toprint[i]->collisionCount << ") ";
+	}
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
